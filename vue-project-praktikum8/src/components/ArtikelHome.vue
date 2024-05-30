8cab4653fd26da7abd144cbd7936217e65db3d04
Yunan WU <yunandrawu@students.amikom.ac.id>
2024-05-30 17:10:07 +0700

    belajar vue framework

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="div">
+        <img src="../assets/hqdefault.jpg" alt="" style="height: 300px;">
+        <h1>Ini adalah komponen artikel</h1>
+        <p>Ini adalah isi artikel</p>
+        <p>Ini adalah isi artikel</p>
+        <p>Ini adalah isi artikel</p>
+        <p>Ini adalah isi artikel</p>
+    </div>
+</template>
---
+++ vue-project-praktikum8/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ArtikelHome from '../components/ArtikelHome.vue'
+
+</script>
+
+<template>
+  
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1>Custom Halaman</h1>
+    <ArtikelHome />
+  </main>
+</template>
